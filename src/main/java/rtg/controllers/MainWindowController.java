dbf3ed1ab2ce92099482f20c72c8e1c955ef751b
Jakub Bogdan <kubab1998@o2.pl>
2020-07-14 16:41:32 +0200

    Pictures added

--- src/main/java/rtg/controllers/MainWindowController.java
+++ src/main/java/rtg/controllers/MainWindowController.java
@@ -5,6 +5,9 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import rtg.exceptions.FileOperationsException;
 import rtg.model.Point;
 import rtg.repositories.PointRepository;
@@ -20,6 +23,8 @@ public class MainWindowController implements Initializable {
     @FXML
     private ListView<Point> pointListView;
     private ObservableList<Point> pointList;
+    @FXML
+    private GridPane tableView;
 
     public MainWindowController() {
         this.pointRepository = new PointRepository();
@@ -32,10 +37,10 @@ public class MainWindowController implements Initializable {
         }
     }
 
-    @FXML
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initPointList();
+        initPictures();
     }
 
     private void initPointList() {
@@ -43,4 +48,15 @@ public class MainWindowController implements Initializable {
         pointListView.setCellFactory(pointListView -> new ListViewCell());
     }
 
+    private void initPictures() {
+        ImageView[] images = new ImageView[4];
+        images[0] = new ImageView(new Image(getClass().getResourceAsStream("/rtg/picture.png")));
+        images[1] = new ImageView(new Image(getClass().getResourceAsStream("/rtg/picture.png")));
+        images[2] = new ImageView(new Image(getClass().getResourceAsStream("/rtg/picture.png")));
+        images[3] = new ImageView(new Image(getClass().getResourceAsStream("/rtg/picture.png")));
+        tableView.add(images[0], 0, 0);
+        tableView.add(images[1], 0, 1);
+        tableView.add(images[2], 1, 0);
+        tableView.add(images[3], 1, 1);
+    }
 }
